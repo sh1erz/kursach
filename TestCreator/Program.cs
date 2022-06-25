e2e0f0eb923754f3ede7988d9fcc20f1b4d6dde2
Torden <assassing13@gmail.com>
2022-06-25 15:03:06 +0300

    Instantiate new class

---
+++ TestCreator/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TestCreator;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
