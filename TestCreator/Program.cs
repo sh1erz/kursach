e55667d23ae1f6728bcb270d3295653ada98d6d2
Karyna Rebrishcheva <karyna.rebrishcheva@yalantis.net>
2022-06-26 15:29:22 +0300

    sentence

--- TestCreator/Program.cs
+++ TestCreator/Program.cs
@@ -10,7 +10,6 @@ internal static class Program
 {
     public static async Task Main(string[] args)
     {
-        var r = new Random();
         var variableVariants = await ReadVariablesFromJsonFile("variables.json");
         var variableProvider = new VariableProvider(variableVariants);
 
@@ -18,11 +17,11 @@ internal static class Program
         var sentence1VariantsDict = await ReadVariantsFromJsonFile("sentence1.json");
         var sentence1Parts = new List<string>();
 
+        var r = new Random();
         foreach (var part in sentence1VariantsDict)
         {
             var values = part.Value;
             var randInt = r.Next(values.Count);
-
             var selectedVariant = values[randInt];
             selectedVariant = string.Format(selectedVariant, variableProvider.Expenses);
             sentence1Parts.Add(selectedVariant);
