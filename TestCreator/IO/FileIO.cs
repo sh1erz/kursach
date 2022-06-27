3fa0e63e252eeb9f4bcf8a1d883bde6f8951b954
Karyna Rebrishcheva <karyna.rebrishcheva@yalantis.net>
2022-06-27 22:04:53 +0300

    Finish dev

--- TestCreator/IO/FileIO.cs
+++ TestCreator/IO/FileIO.cs
@@ -1,18 +1,12 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
-using TestCreator.Models;
 
 namespace TestCreator.IO;
 
-public static class FileReader
+// ReSharper disable once InconsistentNaming
+public static class FileIO
 {
-    public static async Task<string[]> ReadTextFromFile(string filename)
-    {
-        return await File.ReadAllLinesAsync("Json/" + filename);
-    }
-
     public static async Task<T> ReadFromJsonFile<T>(string filename)
     {
         using var r = new StreamReader("Json/" + filename);
@@ -20,5 +14,11 @@ public static class FileReader
         var variants = JsonSerializer.Deserialize<T>(json,
             new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
         return variants;
+    }
+
+    public static async Task WriteToTextFile(string output)
+    {
+        await using StreamWriter file = new("Problems.txt", true);
+        await file.WriteAsync(output);
     }
 }
--- TestCreator/Program.cs
+++ TestCreator/Program.cs
@@ -1,31 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using TestCreator.Generators;
+using TestCreator.IO;
 using TestCreator.Models;
+using Task = System.Threading.Tasks.Task;
 
 namespace TestCreator;
 
 internal static class Program
 {
+    private const int Sentences = 4;
+
     public static async Task Main(string[] args)
     {
-        var variableVariants = await ReadFromJsonFile<VariableVariants>("variables.json");
+        var variableVariants = await FileIO.ReadFromJsonFile<VariableVariants>("variables.json");
         var variableProvider = new VariableProvider(variableVariants);
 
         var sentenceVariants = new List<Dictionary<string, List<string>>>();
-        for (int i = 0; i < Sentences; i++)
+        for (var i = 0; i < Sentences; i++)
         {
-            var sentenceVariantsDict = await ReadFromJsonFile<Dictionary<string, List<string>>>($"sentence{i + 1}.json");
+            var sentenceVariantsDict =
+                await FileIO.ReadFromJsonFile<Dictionary<string, List<string>>>($"sentence{i + 1}.json");
             sentenceVariants.Add(sentenceVariantsDict);
         }
 
-        var variableProvider = new VariableProvider(variableVariants);
         var generator = new SentenceGenerator(variableProvider);
+        var problemDescription = generator.FormatSentences(sentenceVariants);
+        var mathModel = generator.FormatMathModel();
 
-        Console.WriteLine(generator.GenerateSentences(sentenceVariants));
-        Console.WriteLine(generator.PrintMathModel());
+        await FileIO.WriteToTextFile(problemDescription);
+        await FileIO.WriteToTextFile(mathModel);
     }
-
-    public const int Sentences = 4;
 }
